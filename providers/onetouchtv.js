b42fb7722f3ae434779b0e55559714bc643898ca
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-07 22:33:21 +0000

    Update onetouchtv.js

--- providers/onetouchtv.js
+++ providers/onetouchtv.js
@@ -1,59 +1,70 @@
-const axios = require("axios");
+const cheerio = require('cheerio-without-node-native');
 
-const PROVIDER_NAME = "OneTouchTV";
+const PLAYER_BASE = "https://s1.devcorp.me/player/player.html";
 
-async function getStreams(tmdbId, mediaType, season, episode) {
-  try {
+function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
+    return new Promise((resolve) => {
 
-    let title;
+        const tmdbUrl = `https://api.themoviedb.org/3/${mediaType}/${tmdbId}?api_key=b030404650f279792a8d3287232358e3`;
 
-    if (mediaType === "movie") {
-      title = `tmdb-${tmdbId}`;
-    } else {
-      title = `tmdb-${tmdbId}-s${season}e${episode}`;
-    }
+        fetch(tmdbUrl)
+        .then(res => res.json())
+        .then(tmdb => {
 
-    const playerUrl =
-      `https://s1.devcorp.me/player/player.html?title=${title}`;
+            const title = tmdb.title || tmdb.name || tmdb.original_title;
 
-    const res = await axios.get(playerUrl, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "https://s1.devcorp.me/"
-      }
+            let playerUrl;
+
+            if (mediaType === "movie") {
+                playerUrl = `${PLAYER_BASE}?title=${encodeURIComponent(title)}`;
+            } else {
+                playerUrl = `${PLAYER_BASE}?title=${encodeURIComponent(title)}%20S${seasonNum}%20E${episodeNum}`;
+            }
+
+            return fetch(playerUrl);
+        })
+        .then(res => res.text())
+        .then(html => {
+
+            const streams = [];
+
+            const match = html.match(/file=\[(.*?)\]/);
+
+            if (!match) {
+                resolve([]);
+                return;
+            }
+
+            const servers = JSON.parse(`[${match[1]}]`);
+
+            servers.forEach(server => {
+
+                if (!server.file) return;
+
+                streams.push({
+                    name: "OneTouchTV",
+                    title: server.title || "Server",
+                    url: server.file,
+                    quality: "Auto",
+                    headers: {
+                        Referer: "https://s1.devcorp.me/",
+                        Origin: "https://s1.devcorp.me"
+                    },
+                    provider: "onetouchtv"
+                });
+
+            });
+
+            resolve(streams);
+
+        })
+        .catch(() => resolve([]));
+
     });
-
-    const html = res.data;
-
-    const match = html.match(/file=\[(.*?)\]/);
-
-    if (!match) return [];
-
-    const json = JSON.parse(`[${match[1]}]`);
-
-    const streams = [];
-
-    json.forEach((server) => {
-      if (!server.file) return;
-
-      streams.push({
-        url: server.file,
-        quality: server.title || "HD",
-        type: "hls",
-        headers: {
-          Referer: "https://s1.devcorp.me/"
-        }
-      });
-    });
-
-    return streams;
-
-  } catch (e) {
-    return [];
-  }
 }
 
-module.exports = {
-  name: PROVIDER_NAME,
-  getStreams
-};
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { getStreams };
+} else {
+    global.getStreams = getStreams;
+}
