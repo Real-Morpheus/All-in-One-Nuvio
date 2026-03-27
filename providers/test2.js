cfb15955986b8d5dc0feafca447f54b6423355ad
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-27 08:55:19 +0000

    Update test2.js

--- providers/test2.js
+++ providers/test2.js
@@ -1,9 +1,9 @@
 "use strict";
 
 var PROVIDER_NAME = "OnlyKDrama";
-var SITE_URL = "https://onlykdrama.top";
+var SITE_URL = "https://asiaflix.org";
 var TMDB_URL = "https://www.themoviedb.org";
-var FILEPRESS_ORIGIN = "https://pixeldrain.dev";
+var FILEPRESS_ORIGIN = "https://hugh.cdn.rumble.cloud/";
 var DEFAULT_HEADERS = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
   "Accept-Language": "en-US,en;q=0.9"
